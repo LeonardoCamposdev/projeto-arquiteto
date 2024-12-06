522b88526ed85ac81d409c2a4d67809f3291f207
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-12-05 21:59:52 -0300

    up

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,12 +1,14 @@
 export default function initMenuMobile(){
-
+  const buttonMenu = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const html = document.querySelector('body');
+  
+  function handleClick(){
+    buttonMenu.classList.toggle('active');
+    menuList.classList.toggle('active');
+  }
+  
+  buttonMenu.addEventListener('click',handleClick);
+  menuList.addEventListener('click',handleClick);
 }
 
-const buttonMenu = document.querySelector('[data-menu="button"]');
-const menuList = document.querySelector('[data-menu="list"]');
-
-function handleClick(){
-  menuList.classList.add('active');
-}
-
-buttonMenu.addEventListener('click',handleClick);
